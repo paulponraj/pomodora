6eb18aec88cc6e54e0b52ff967e9310d197dd188
Paul Pon Raj <106603283+paulponraj@users.noreply.github.com>
2022-08-10 18:18:20 +0530

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,16 @@ import { FunctionComponent } from 'react'
 const Footer: FunctionComponent = () => {
   return (
     <div className="flex justify-center text-slate-500 p-10 ">
-    <div>© 2022 Ananya Dhananjaya. All Rights Reserved.<href="https://www.copypassword.com/"></div>
+    <p className="-mt-8 text-center text-gray-300">
+          Made with ♡ by{' '}
+          <a
+            href="https://saurish.com/?utm_source=uranium"
+            className="underline"
+          >
+            Saurish
+          </a>
+          . &copy; Uranium 2022. All rights reserved.
+        </p>
     </div>
   )
 }
