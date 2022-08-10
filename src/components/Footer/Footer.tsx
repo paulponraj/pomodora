ae335df3f8721f10f9b631f314f0cf98e0456432
Paul Pon Raj <106603283+paulponraj@users.noreply.github.com>
2022-08-10 18:41:03 +0530

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,17 +4,15 @@ const Footer: FunctionComponent = () => {
   return (
     <div className="flex justify-center text-slate-500 p-10 ">
     <p className="-mt-8 text-center text-black-300">
-          Made with ♡ by{' '}
+          Click here to access{' '}
           <a
-            href="https://saurish.com/?utm_source=uranium"
+            href="https://mydash.fun/"
             className="underline"
           >
-            Saurish
+            Dashboard
           </a>
-          . &copy; Uranium 2022. All rights reserved.
-      <script async src="https://cse.google.com/cse.js?cx=1236b84bea0d14837">
-                         </script>
-        </p>
+          . &copy; Paul Pon Raj 2022. All rights reserved.
+     </p>
     </div>
   )
 }
