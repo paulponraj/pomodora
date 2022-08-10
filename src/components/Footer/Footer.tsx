c24ac61a962b468362ebda5d8f3e075b8eebfb2b
Paul Pon Raj <106603283+paulponraj@users.noreply.github.com>
2022-08-10 18:13:16 +0530

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import { FunctionComponent } from 'react'
 const Footer: FunctionComponent = () => {
   return (
     <div className="flex justify-center text-slate-500 p-10 ">
-    <div>© 2022 Ananya Dhananjaya. All Rights Reserved.</div><a href="https://www.copypassword.com/">
+    <div>© 2022 Ananya Dhananjaya. All Rights Reserved.<a href="https://www.copypassword.com/"></div>
     </div>
   )
 }
