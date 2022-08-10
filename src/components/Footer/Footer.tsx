bdc710f0eee388896192c5363b13d881594f2ede
Paul Pon Raj <106603283+paulponraj@users.noreply.github.com>
2022-08-10 18:10:16 +0530

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,8 +3,7 @@ import { FunctionComponent } from 'react'
 const Footer: FunctionComponent = () => {
   return (
     <div className="flex justify-center text-slate-500 p-10 ">
-    <a href="https://www.copypassword.com/"> <h2 class="font-bold uppercase text-gray-600">Blue Pass</h2> 
-    <a href="https://www.copypassword.com/" class="small-box-footer">Generate unique & strong passwords <i class="fas fa-arrow-circle-right"></i></a>
+    <a href="https://www.copypassword.com/"></h2>Blue Pass</h2>
     </div>
   )
 }
