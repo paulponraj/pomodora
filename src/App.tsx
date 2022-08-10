acdbcb88085e85152fc9bd07f784aa0921951832
Paul Pon Raj <106603283+paulponraj@users.noreply.github.com>
2022-08-10 17:45:29 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react'
 import AppBar from './components/AppBar'
 import BreakComponent from './components/BreakComponent/BreakComponent'
-import Footer from './components/Footer'
 
 function App() {
   const [theme, setTheme] = useState<'light' | 'dark'>('light')
@@ -18,13 +17,6 @@ function App() {
     <div className="md:px-14 lg:px-36 xl:px-80 h-screen bg-slate-50 dark:bg-slate-900">
       <AppBar theme={theme} setTheme={setTheme} />
       <BreakComponent />
-      <Footer />
-      <div class="flex flex-1 md:w-1/3 justify-center md:justify-start text-black px-2">
-                <span class="relative w-full">
-                <script async src="https://cse.google.com/cse.js?cx=1236b84bea0d14837">
-                         </script>
-                    <div class="gcse-search"></div>
-    </div>
   )
 }
 
