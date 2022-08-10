f3fa9057723561cc35e732e31a4d6e583e810a82
Paul Pon Raj <106603283+paulponraj@users.noreply.github.com>
2022-08-10 17:44:43 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,11 @@ function App() {
       <AppBar theme={theme} setTheme={setTheme} />
       <BreakComponent />
       <Footer />
+      <div class="flex flex-1 md:w-1/3 justify-center md:justify-start text-black px-2">
+                <span class="relative w-full">
+                <script async src="https://cse.google.com/cse.js?cx=1236b84bea0d14837">
+                         </script>
+                    <div class="gcse-search"></div>
     </div>
   )
 }
