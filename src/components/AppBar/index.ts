0e38fb3b9e4df424abe983fcd7dc79cae0ee95a4
Paul Pon Raj <106603283+paulponraj@users.noreply.github.com>
2022-08-10 12:47:19 +0530

    Update index.ts

--- src/components/AppBar/index.ts
+++ src/components/AppBar/index.ts
@@ -1,1 +1,4 @@
+<script async src="https://cse.google.com/cse.js?cx=1236b84bea0d14837">
+                         </script>
+                    <div class="gcse-search"></div>
 export { default } from './AppBar'
