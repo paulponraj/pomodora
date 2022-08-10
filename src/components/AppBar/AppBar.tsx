785db060fe1f56e8e2bdb87f7c24c33a16d1dfb1
Paul Pon Raj <106603283+paulponraj@users.noreply.github.com>
2022-08-10 12:43:29 +0530

    Update AppBar.tsx

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -27,9 +27,15 @@ const AppBar: FunctionComponent<Props> = (props: Props) => {
           >
             {theme === 'light' ? <FaRegMoon size={24} /> : <BiSun size={24} />}
           </div>
+          <div class="flex flex-1 md:w-1/3 justify-center md:justify-start text-black px-2">
+                <span class="relative w-full">
+                <script async src="https://cse.google.com/cse.js?cx=1236b84bea0d14837">
+                         </script>
+                    <div class="gcse-search"></div>
         </div>
       </div>
     </div>
+    
   )
 }
 
