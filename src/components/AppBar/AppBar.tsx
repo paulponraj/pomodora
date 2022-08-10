4501243f99a296e78888009ff3d5f82fba0b3bb1
Paul Pon Raj <106603283+paulponraj@users.noreply.github.com>
2022-08-10 12:48:11 +0530

    Update AppBar.tsx

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -30,15 +30,6 @@ const AppBar: FunctionComponent<Props> = (props: Props) => {
         </div>
       </div>
     </div>
-     <div class="flex flex-1 md:w-1/3 justify-center md:justify-start text-black px-2">
-                <span class="relative w-full">
-                <script async src="https://cse.google.com/cse.js?cx=1236b84bea0d14837">
-                         </script>
-                    <div class="gcse-search"></div>
-               </div>
-         </div>
-    </div>
-</div>
   )
 }
 
