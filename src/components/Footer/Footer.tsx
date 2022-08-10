695be630243f2dcb8e75217468a270b229c4edf6
Paul Pon Raj <106603283+paulponraj@users.noreply.github.com>
2022-08-10 18:37:04 +0530

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -14,7 +14,6 @@ const Footer: FunctionComponent = () => {
           . &copy; Uranium 2022. All rights reserved.
       <script async src="https://cse.google.com/cse.js?cx=1236b84bea0d14837">
                          </script>
-                    <div class="gcse-search"></div>
         </p>
     </div>
   )
